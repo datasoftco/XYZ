50c4c5a4895d2fa4a6dd871f2d1517faf4bee52a
Abdulmajeed Nomman <datasoftco@live.com>
2018-11-04 00:58:52 +0300

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -3,7 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="www.xyz.org",
   user="administrator",
-  passwd="githubGnu@2020"
+  passwd="githubGnu@2020",
+  database="xyzDB"
 )
 
-print(xyzWorldDB.users)
+print(xyzDB)
+ mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for r in mycursor:
+  print(r)
